bfc8de8dd6b25e005df542bffd68b9947bdda2b9
Cristian Incarnato <cristian.cdi@gmail.com>
2022-02-03 22:03:36 -0300

    Initial commit

---
+++ apps/backend/__tests__/templates/template.test.js
@@ -0,0 +1,18 @@
+
+describe("template", () => {
+
+
+    beforeAll(async () => {
+        jest.setTimeout(10000)
+    });
+
+    afterAll(async () => {
+
+    })
+
+
+    test('template true', async () => {
+        expect(true).toBe(true)
+    })
+
+})
