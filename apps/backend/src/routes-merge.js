226c38731beef593e98bf2fe6ae962a5c1bb26e1
Cristian Incarnato <cristian.cdi@gmail.com>
2022-02-10 12:21:09 -0300

    Add Swagger

--- apps/backend/src/routes-merge.js
+++ apps/backend/src/routes-merge.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 import DockerRoutes from './modules/docker/routes/DockerRoutes'
 import GitlabRoutes from './modules/gitlab/routes/GitlabRoutes'
 import RegistryRoutes from './modules/registry/routes/RegistryRoutes'
 let router = express.Router();
 
+const swaggerDocument = require('./swagger.json')
+
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument) )
 router.use('/', DockerRoutes);
 router.use('/', GitlabRoutes);
 router.use('/', RegistryRoutes);
