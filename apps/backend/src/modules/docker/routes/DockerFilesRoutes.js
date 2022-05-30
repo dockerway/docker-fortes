8b29b78617af5b2e5dc74e13f012812af4e6a0d5
Carolina Romero <carolina.romero@sondeos.com.ar>
2022-05-30 14:53:41 -0300

    Config File

--- apps/backend/src/modules/docker/routes/DockerFilesRoutes.js
+++ apps/backend/src/modules/docker/routes/DockerFilesRoutes.js
@@ -1,15 +1,18 @@
 import express from 'express'
 const fs = require('fs');
+const path = require('path');
 
 var router = express.Router();
 
 router.post('/docker/files', async function (req, res) {
     try {
         if(!Array.isArray(req.body)) throw new Error("Request body must be an Array!")
-        
+
         for(let i = 0; i < req.body.length; i++){
             if(!req.body[i].fileName || !req.body[i].fileContent || !req.body[i].containerPath) throw new Error("One of the properties of the file is not defined.")
-            
+
+            await createDirIfDoesntExist(req.body[i].containerPath) //create the directory            
+
             let pathFile = req.body[i].containerPath + req.body[i].fileName
             fs.writeFile(pathFile,req.body[i].fileContent,{flag:'w'}, (err) => {
                 if(err) throw err;
@@ -24,5 +27,10 @@ router.post('/docker/files', async function (req, res) {
         res.send(e.message)
     }
 })
+
+const createDirIfDoesntExist = function(dst){
+    let dir = path.dirname(dst)
+    fs.promises.mkdir(dir, { recursive: true });
+}
 
 module.exports = router;
