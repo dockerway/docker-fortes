27de33fde65bb98223c289682e7a4c10bc966471
Carolina Romero <carolina.romero@sondeos.com.ar>
2022-06-22 09:34:40 -0300

    Creación de carpetas

--- apps/backend/src/modules/docker/routes/DockerFolderCreator.js
+++ apps/backend/src/modules/docker/routes/DockerFolderCreator.js
@@ -7,9 +7,9 @@ router.post('/docker/folders', async function (req, res) {
     try {
         console.log("BODY: ",req.body)
         if(!Array.isArray(req.body)) throw new Error("Request body must be an Array!")
-        let res = await foldersCreator(req.body)
-        res.status(200)
-        res.json(res)
+        let r = await foldersCreator(req.body)
+        console.log("body folders: ",req.body)
+        res.json(r)
     } catch(e){
         console.error("Error DockerFolderCreator: ",e)
         res.status(500)
