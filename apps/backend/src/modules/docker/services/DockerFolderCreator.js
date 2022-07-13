cc118705c69647ee2e01f0368ea604c4ec1e5840
Carolina Romero <carolina.romero@sondeos.com.ar>
2022-07-13 09:27:00 -0300

    Refactor create volumes

--- apps/backend/src/modules/docker/services/DockerFolderCreator.js
+++ apps/backend/src/modules/docker/services/DockerFolderCreator.js
@@ -3,7 +3,7 @@ import { dnsTaskRunningByServiceAndNode } from "./DockerTaskService";
 import axios from "axios";
 import { DefaultLogger as winston} from "@dracul/logger-backend"
 
-export const foldersCreator = function (body) {
+export const foldersCreator = function (volumes) {
     return new Promise(async (resolve, reject) => {
         try {
             const DEFAULT_AGENT_SERVICE_NAME = "dockerway_incatainer-agent"
@@ -17,7 +17,7 @@ export const foldersCreator = function (body) {
                 try {
                     let baseURL = "http://"+ await dnsTaskRunningByServiceAndNode(agentServiceName, nodes[i].id)
                     let URL = baseURL + path
-                    let response = await axios.post(URL, body)
+                    let response = await axios.post(URL, volumes)
                     if(response.status == 200) successCounter++
                 } catch(e){
                     winston.error("ERROR foldersCreator node: " + nodes[i], e)
