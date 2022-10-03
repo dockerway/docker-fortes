bdbb1d659013f34498000699f96f311839ee6391
ramirosandoval <ramiro_sando@outlook.com>
2022-10-03 11:02:41 -0300

    Traduccion de permisos de roles añadidos en commit anterior

---
+++ apps/frontend/src/modules/docker/i18n/messages/RoleMessages.js
@@ -0,0 +1,47 @@
+const messages = {
+    en: {
+        role: {
+            permissions: {
+                DOCKER_CONSOLE:"Docker console usage",
+                DOCKER_LOGS:"Docker logs visualization",
+                DOCKER_CREATE:"Docker service creation",
+                DOCKER_VIEW:"Docker service visualization",
+                DOCKER_REMOVE:"Docker service deletion",
+                DOCKER_RESTART:"Docker services restart",
+                DOCKER_UPDATE:"Docker services updation",
+                AUDIT_UPDATE:"Audit updation",
+            }
+        },
+    },
+    es: {
+        role: {
+            permissions: {
+                DOCKER_CONSOLE:"Uso de consola Docker",
+                DOCKER_LOGS:"Visualización de logs Docker",
+                DOCKER_CREATE:"Creacion de servicio Docker",
+                DOCKER_VIEW:"Visualización de servicios Docker",
+                DOCKER_REMOVE:"Eliminación de servicios Docker",
+                DOCKER_RESTART:"Reinicio de servicios Docker",
+                DOCKER_UPDATE:"Actualizacion de servicios Docker",
+                AUDIT_UPDATE:"Actualizacion de Auditoria",
+            }
+        },
+
+    },
+    pt: {
+        role: {
+            permissions: {
+                DOCKER_CONSOLE:"Console do Docker",
+                DOCKER_LOGS:"Logs do serviços Docker",
+                DOCKER_CREATE:"Criação do serviços Docker",
+                DOCKER_VIEW:"Visor do serviços Docker",
+                DOCKER_REMOVE:"Exclusão do serviços Docker",
+                DOCKER_RESTART:"Reiniciar o serviços Docker",
+                DOCKER_UPDATE:"Atualização do serviços Docker",
+                AUDIT_UPDATE:"Atualização de Auditoria",
+                }
+            }
+        },
+}
+
+export default messages
