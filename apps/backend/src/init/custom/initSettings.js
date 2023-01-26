9008354b73ab36f859105296e0511cfea306169d
bloome-alex <alex99valdiviezo@gmail.com>
2023-01-26 18:06:28 -0300

    maxLogsLines setting creation - #74186

--- apps/backend/src/init/custom/initSettings.js
+++ apps/backend/src/init/custom/initSettings.js
@@ -9,7 +9,12 @@ const settings = [
         type: 'number',
         label: {en: 'ENENEN', es: 'ESESES', pt: 'PTPTPT'}
     },*/
-
+    {
+        key: 'maxLogsLines',
+        value: 10000,
+        type: 'number',
+        label: {en: 'Maximum quantity log lines', es: 'Cantidad máxima de lineas de logs', pt: 'máximo de linhas de logs'}   
+    }
 ]
 
 export const initSettings = async function () {
