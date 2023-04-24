4f977c03ffc37b5cd71c1559c5a0f4ac2a4c7469
ramiro.sandoval <ramiro_sando@outlook.com>
2023-04-24 17:50:43 -0300

    #78541

--- apps/backend/src/modules/docker/routes/DockerFolderCreator.js
+++ apps/backend/src/modules/docker/routes/DockerFolderCreator.js
@@ -1,23 +1,25 @@
-import express from 'express'
-import { foldersCreator } from '../services/DockerFolderCreator'
+import { foldersCreator } from '../services/DockerFolderCreator';
 import { DOCKER_UPDATE } from '../permissions/dockerPermissions';
+import express from 'express';
 
-var router = express.Router();
+
+const router = express.Router()
 
 router.post('/docker/folders', async function (req, res) {
     try {
-        const user = req.user;
-        if(!user)  throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto");
-        if(!req.rbac.isAllowed(user.id, DOCKER_UPDATE)) throw new ForbiddenError("Not Authorized");
+        const user = req.user
+        if (!user) throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
+        if (!req.rbac.isAllowed(user.id, DOCKER_UPDATE)) throw new ForbiddenError("Not Authorized")
         
-        if(!Array.isArray(req.body)) throw new Error("Request body must be an Array!")
-        let r = await foldersCreator(req.body)
-        res.status(200)
-        res.json(r)
-    } catch(e){
-        console.error("Error DockerFolderCreator: ",e)
-        res.status(500)
-        res.send(e.message)
+        if (!Array.isArray(req.body) )throw new Error("Request body must be an Array!")
+
+        const foldersCreatorResponse = await foldersCreator(req.body)
+        console.log(`volumes: '${req.body}'`)
+
+        res.status(200).json(foldersCreatorResponse)
+    } catch(error){
+        console.error(`An error happened while we tried to use the foldersCreator function: '${error}'`)
+        res.status(500).send(error.message)
     }
 })
 
--- apps/backend/src/modules/docker/services/DockerFolderCreator.js
+++ apps/backend/src/modules/docker/services/DockerFolderCreator.js
@@ -1,33 +1,39 @@
-import { fetchNode } from './DockerNodeService'
 import { dnsTaskRunningByServiceAndNode } from "./DockerTaskService";
+import { DefaultLogger as winston } from "@dracul/logger-backend";
+import { fetchNode } from './DockerNodeService';
 import axios from "axios";
-import { DefaultLogger as winston} from "@dracul/logger-backend"
 
-export const foldersCreator = function (volumes) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const DEFAULT_AGENT_SERVICE_NAME = "dockerway_incatainer-agent"
-            const agentServiceName = process.env.AGENT_SERVICE_NAME ? process.env.AGENT_SERVICE_NAME : DEFAULT_AGENT_SERVICE_NAME
+export const foldersCreator = async function (volumes) {
+    try {
+        const DEFAULT_AGENT_SERVICE_NAME = "dockerway_incatainer-agent"
+        const agentServiceName = process.env.AGENT_SERVICE_NAME ? process.env.AGENT_SERVICE_NAME : DEFAULT_AGENT_SERVICE_NAME
 
-            let nodes = await fetchNode()
-            let path = '/api/docker/folders'
-            let successCounter = 0
+        const nodes = await fetchNode()
+        const path = '/api/docker/folders'
 
-            for(let i = 0; i < nodes.length; i++){
-                try {
-                    let baseURL = "http://"+ await dnsTaskRunningByServiceAndNode(agentServiceName, nodes[i].id)
-                    let URL = baseURL + path
-                    let response = await axios.post(URL, volumes)
-                    if(response.status == 200) successCounter++
-                } catch(e){
-                    winston.error("ERROR foldersCreator node: " + nodes[i], e)
-                }
+        let successCounter = 0
+
+        for (let i = 0; i < nodes.length; i++) {
+            try {
+                const baseURL = "http://" + await dnsTaskRunningByServiceAndNode(agentServiceName, nodes[i].id)
+                const URL = baseURL + path
+                const response = await axios.post(URL, volumes)
+
+                console.log(`response: '${response}'`)
+                if (response.status == 200) successCounter += 1
+            } catch (error) {
+                winston.error("ERROR foldersCreator node: " + nodes[i], error)
             }
-
-            winston.info("Nodes created successfully: " + successCounter + "/" + nodes.length)
-            resolve({ nodes: nodes.length, success: successCounter })
-        } catch (e) {
-            reject(e)
         }
-    })
+
+        winston.info("Nodes created successfully: " + successCounter + "/" + nodes.length)
+        return (
+            {
+                nodes: nodes.length,
+                success: successCounter
+            }
+        )
+    } catch (error) {
+        throw (error)
+    }
 }
