3aaba69851d090e71597a5c78028e772805eb826
ramiro.sandoval <ramiro_sando@outlook.com>
2023-04-27 08:33:32 -0300

    end of #78541

--- apps/backend/src/modules/docker/services/DockerFolderCreator.js
+++ apps/backend/src/modules/docker/services/DockerFolderCreator.js
@@ -12,28 +12,34 @@ export const foldersCreator = async function (volumes) {
 
         let successCounter = 0
 
+        let neededDirectoriesAreMountedInFortesAgent = null
+        const notMountedMessage = 'The needed directories are not mounted; please contact your infrastructure team!' //has to match the agent's notMountedMessage
+
         for (let i = 0; i < nodes.length; i++) {
             try {
                 const baseURL = "http://" + await dnsTaskRunningByServiceAndNode(agentServiceName, nodes[i].id)
                 const path = '/api/docker/folders'
-                
+
                 const URL = baseURL + path
                 const response = await axios.post(URL, volumes)
 
-                console.log(`response: '${response}'`)
-                if (response.status == 200) successCounter += 1
+                winston.info(`agent folders endpoint response: '${JSON.stringify(response.data)}'`)
+
+                if (response.data.response == notMountedMessage) {
+                    neededDirectoriesAreMountedInFortesAgent = false
+                }else if (response.status == 200) {
+                    successCounter += 1
+                    neededDirectoriesAreMountedInFortesAgent = true
+                }
+
             } catch (error) {
-                winston.error("ERROR foldersCreator node: " + nodes[i], error)
+                winston.error("ERROR foldersCreator node: " + JSON.stringify(nodes[i]), error)
             }
+
+            winston.info("Nodes created successfully: " + successCounter + "/" + nodes.length)
         }
 
-        winston.info("Nodes created successfully: " + successCounter + "/" + nodes.length)
-        return (
-            {
-                nodes: nodes.length,
-                success: successCounter
-            }
-        )
+        return (neededDirectoriesAreMountedInFortesAgent) ? { nodes: nodes.length, success: successCounter } : notMountedMessage
     } catch (error) {
         throw (error)
     }
