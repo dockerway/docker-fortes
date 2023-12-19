1c44200b2ac4a4597ca4f2869df5abe09e093a37
ramirosandoval <ramiro_sando@outlook..com>
2023-12-19 03:00:57 -0300

    feat(DockerServiceRoutes): added DELETE endpoint and refactored all endpooints to avoid code repetition by using new dracul access backend middleware for authentication and authorization.

--- apps/backend/src/modules/docker/routes/DockerServiceRoutes.js
+++ apps/backend/src/modules/docker/routes/DockerServiceRoutes.js
@@ -1,4 +1,9 @@
 import express from 'express'
+import http from "http";
+import { requireAuthentication, requireAuthorization } from '@dracul/access-backend';
+import { DOCKER_CREATE, DOCKER_REMOVE, DOCKER_UPDATE, DOCKER_VIEW } from '../permissions/dockerPermissions';
+import { serviceStats, serviceStatsByName } from "../services/DockerStatsService";
+import { dockerRemove } from '../services/DockerManageService';
 import {
     dockerServiceCreate,
     dockerServiceUpdate,
@@ -6,24 +11,17 @@ import {
     findServiceByIdOrName,
     findServiceTag
 } from "../services/DockerService";
-import http from "http";
-import { serviceStats, serviceStatsByName } from "../services/DockerStatsService";
-import { AuthenticationError, ForbiddenError } from 'apollo-server-express';
-import { DOCKER_CREATE, DOCKER_UPDATE, DOCKER_VIEW } from '../permissions/dockerPermissions';
 
 
-let router = express.Router();
-router.use(express.json());
+let router = express.Router()
+router.use(express.json())
 
-function validateStatusCode(statusCode){
+function validateStatusCode(statusCode) {
     return http.STATUS_CODES.hasOwnProperty(statusCode)
 }
 
-router.get('/docker/service', async function (req, res) {
+router.get('/docker/service',[requireAuthentication, requireAuthorization([DOCKER_VIEW])],async function (req, res) {
     try {
-        if(!req.user)  throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if(!req.rbac.isAllowed(req.user.id, DOCKER_VIEW)) throw new ForbiddenError("Not Authorized")
-
         res.json(await fetchService())
     } catch (error) {
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
@@ -31,12 +29,8 @@ router.get('/docker/service', async function (req, res) {
     }
 })
 
-
-router.post('/docker/service', async function (req, res) {
+router.post('/docker/service',[requireAuthentication, requireAuthorization([DOCKER_CREATE])],async function (req, res) {
     try {
-        if (!req.user) throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if (!req.rbac.isAllowed(req.user.id, DOCKER_CREATE)) throw new ForbiddenError("Not Authorized")
-
         res.json(await dockerServiceCreate(req.user, req.body))
     } catch (error) {
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
@@ -49,16 +43,13 @@ router.post('/docker/service', async function (req, res) {
                 res.status(statusCode).send(`The service already existed and an error happened when we tried to update it: '${error.message}'`)
             }
         } else {
-            res.status(statusCode).send(error.message)    
+            res.status(statusCode).send(error.message)
         }
     }
 })
 
-router.put('/docker/service/:service', async function (req, res) {
+router.put('/docker/service/:service',[requireAuthentication, requireAuthorization([DOCKER_UPDATE])],async function (req, res) {
     try {
-        if(!req.user)  throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if(!req.rbac.isAllowed(req.user.id, DOCKER_UPDATE)) throw new ForbiddenError("Not Authorized")
-
         res.json(await dockerServiceUpdate(req.user, req.params.service, req.body))
     } catch (error) {
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
@@ -66,22 +57,27 @@ router.put('/docker/service/:service', async function (req, res) {
     }
 })
 
-router.get('/docker/service/:serviceIdentifier', async function (req, res) {
+router.delete('/docker/service/:service', [requireAuthentication, requireAuthorization([DOCKER_REMOVE])], async function (req, res) {
     try {
-        if(!req.user)  throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if(!req.rbac.isAllowed(req.user.id, DOCKER_VIEW)) throw new ForbiddenError("Not Authorized")
+        const serviceRemovalResult = await dockerRemove(req.user, req.params.service)
+        res.send(serviceRemovalResult).status(200)
+    } catch (error) {
+        winston.error(`An error happened at the service delete endpoint: '${error}'`)
+        res.status(500).send(error.message)
+    }
+})
 
+
+router.get('/docker/service/:serviceIdentifier',[requireAuthentication, requireAuthorization([DOCKER_VIEW])], async function (req, res) {
+    try {
         res.json(await findServiceByIdOrName(req.params.serviceIdentifier))
     } catch (error) {
         res.status(error.message === "Service not found" ? 404 : 500).send(error.message)
     }
 })
 
-router.get('/docker/service/:name/tag', async function (req, res) {
+router.get('/docker/service/:name/tag',[requireAuthentication, requireAuthorization([DOCKER_VIEW])], async function (req, res) {
     try {
-        if(!req.user)  throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if(!req.rbac.isAllowed(req.user.id, DOCKER_VIEW)) throw new ForbiddenError("Not Authorized")
-
         res.json(await findServiceTag(req.params.name))
     } catch (error) {
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
@@ -90,11 +86,8 @@ router.get('/docker/service/:name/tag', async function (req, res) {
 })
 
 
-router.get('/docker/service/:serviceName/stats', async function (req, res) {
+router.get('/docker/service/:serviceName/stats', [requireAuthentication, requireAuthorization([DOCKER_VIEW])], async function (req, res) {
     try {
-        if (!req.user) throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if (!req.rbac.isAllowed(req.user.id, DOCKER_VIEW)) throw new ForbiddenError("Not Authorized")
-
         res.json(await serviceStatsByName(req.params.serviceName))
     } catch (error) {
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
@@ -102,11 +95,8 @@ router.get('/docker/service/:serviceName/stats', async function (req, res) {
     }
 })
 
-router.get('/docker/service/id/:serviceId/stats', async function (req, res) {
+router.get('/docker/service/id/:serviceId/stats',[requireAuthentication, requireAuthorization([DOCKER_VIEW])], async function (req, res) {
     try {
-        if(!req.user)  throw new AuthenticationError("Usted no esta autenticado o su token es incorrecto")
-        if(!req.rbac.isAllowed(req.user.id, DOCKER_VIEW)) throw new ForbiddenError("Not Authorized")
-
         res.json(await serviceStats(req.params.serviceId))
     } catch (error) {
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
