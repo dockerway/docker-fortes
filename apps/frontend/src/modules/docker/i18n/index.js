de11b40de810dd8c75b58e380f95404bab69f778
Alex Valdiviezo <alex99valdiviezo@gmail.com>
2024-03-20 13:29:18 -0300

    feat(122611): ClusterInformation module. Allows you to view the nodes, resources and tasks in real time.

--- apps/frontend/src/modules/docker/i18n/index.js
+++ apps/frontend/src/modules/docker/i18n/index.js
@@ -3,11 +3,13 @@ import merge from 'deepmerge'
 import DockerMessages from './messages/DockerMessages'
 import RoleMessages from './messages/RoleMessages'
 import FiltersMessages from './messages/FiltersMessages'
+import ClusterInformationPageMessages from './messages/ClusterInformationPageMessages'
 
 const messages = merge.all([
     DockerMessages,
     RoleMessages,
-    FiltersMessages
+    FiltersMessages,
+    ClusterInformationPageMessages
 ])
 
 export default messages;
---
+++ apps/frontend/src/modules/docker/i18n/messages/ClusterInformationPageMessages/PrimaryPageMessages.js
@@ -0,0 +1,97 @@
+const messages = {
+    es: {
+        docker: {
+            clusterInformationPage: {
+                primaryPage: {
+                    title: 'Información del cluster',
+                    subtitle: 'Información detallada',
+                    options: {
+                        title: 'Opciones',
+                        subtitle: 'Opciones generales',
+                        onlyDisplayRunningTasks: 'Mostrar solo tareas en ejecución',
+                        displayNodeLabels: 'Mostrar etiquetas de nodo',
+                    },
+                    refresh: {
+                        title: 'Refresco',
+                        subtitle: 'Opciones de refresco',
+                        refreshRate: 'Frecuencia de refresco',
+                        autoRefresh: 'Refresco automático',
+                    },
+                    clusterVisualizer: {
+                        title: 'Visualizador de cluster',
+                        subtitle: 'Visualización'
+                    },
+                    basicInformation: {
+                        nodes: 'Nodos',
+                        services: 'Servicios',
+                        tasks: 'Tareas'
+                    }
+                }
+            }
+        }
+    },
+    en: {
+        docker: {
+            clusterInformationPage: {
+                primaryPage: {
+                    title: 'Cluster information',
+                    subtitle: 'Detailed information',
+                    options: {
+                        title: 'Options',
+                        subtitle: 'General options',
+                        onlyDisplayRunningTasks: 'Only display running tasks',
+                        displayNodeLabels: 'Display node labels',
+                    },
+                    refresh: {
+                        title: 'Refresh',
+                        subtitle: 'Refresh options',
+                        refreshRate: 'Refresh rate',
+                        autoRefresh: 'Auto refresh',
+                    },
+                    clusterVisualizer: {
+                        title: 'Cluster Visualizer',
+                        subtitle: 'Visualization'
+                    },
+                    basicInformation: {
+                        nodes: 'Nodes',
+                        services: 'Services',
+                        tasks: 'Tasks'
+                    }
+                }
+            }
+        }
+    },
+    pt: {
+        docker: {
+            clusterInformationPage: {
+                primaryPage: {
+                    title: 'Informação do cluster',
+                    subtitle: 'Informação detalhada',
+                    options: {
+                        title: 'Opções',
+                        subtitle: 'Opções gerais',
+                        onlyDisplayRunningTasks: 'Apenas mostrar tarefas em execução',
+                        displayNodeLabels: 'Mostrar etiquetas de nodo',
+                    },
+                    refresh: {
+                        title: 'Refrescar',
+                        subtitle: 'Opções de refresco',
+                        refreshRate: 'Frequência de refresco',
+                        autoRefresh: 'Refrescar automático',
+                    },
+                    clusterVisualizer: {
+                        title: 'Visualizador de cluster',
+                        subtitle: 'Visualização'
+                    },
+                    basicInformation: {
+                        nodes: 'Nodos',
+                        services: 'Serviços',
+                        tasks: 'Tarefas'
+                    }
+                }
+            }
+        }
+    }
+}
+
+export default messages
